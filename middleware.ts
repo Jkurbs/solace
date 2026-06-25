9653e468276278704c5ff40142c2ee568fd51d89
Jkurbs <jkurbs18@gmail.com>
2026-06-25 10:23:13 -0400

    Route console subdomain to console

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 const appHosts = new Set(['app.solace.fyi']);
+const consoleHosts = new Set(['console.solace.fyi']);
 
 export function middleware(request: NextRequest) {
   const host = request.headers.get('host')?.split(':')[0].toLowerCase();
@@ -9,6 +10,13 @@ export function middleware(request: NextRequest) {
   if (host && appHosts.has(host) && request.nextUrl.pathname === '/') {
     const url = request.nextUrl.clone();
     url.pathname = '/dashboard';
+
+    return NextResponse.redirect(url);
+  }
+
+  if (host && consoleHosts.has(host) && request.nextUrl.pathname === '/') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/console';
 
     return NextResponse.redirect(url);
   }
